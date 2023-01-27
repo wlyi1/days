00012501d5c84fa6c13bcfe874a0206b172b7711
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-27 09:40:22 +0700

    try to implement log auth ui

--- main.py
+++ main.py
@@ -26,57 +26,70 @@ hari = dt.now().astimezone()
 lahir = datetime.datetime(1996,9,1, tzinfo=tzlocal())
 rdelta = relativedelta(hari, lahir)
 
-#st.title("Aku Telah Hidup")
-#st.success(f'**{rdelta.years} Tahun - {rdelta.months} Bulan - {rdelta.days} Hari - {rdelta.hours + 7} Jam**')
-
-#Place Tet into Image
-
-image = Image.open('frames.png')
-path_font = "Inter-Regular.ttf"
-
-font = ImageFont.truetype(path_font, 200)
-font1 = ImageFont.truetype(path_font, 65)
-
-img= ImageDraw.Draw(image)
-img.text((170,314), str(rdelta.years), font=font, fill=(91,112,143))
-img.text((573,304), str(rdelta.months), font=font1, fill=(91,112,143))
-img.text((573,420), str(rdelta.days), font=font1, fill=(91,112,143))
-img.text((573,539), str(rdelta.hours + 7), font=font1, fill=(91,112,143))
-
-st.image(image)
-
-with st.form(key='form1', clear_on_submit=True):
-    
-    col1 = db.collection('daily')
-    st.write("Apakah aku puas dengan hari ini?")
-    option = st.radio(' ', ('Iya', 'Engga'), horizontal=True)
-    cerita = st.text_area(label='Yang aku dapat hari ini', height=300)
-    submit_button = st.form_submit_button(label='Kirim')
-    if submit_button:
-        col1.add({"option": option, "tanggal": hari, "cerita": cerita})
-        st.write('Terimakasih 👍')
 
 
-doc = db.collection('daily')
-datas = list(doc.stream())
-list_random = list(map(lambda x: x.to_dict(), datas))
-data = pd.DataFrame(list_random)
-data = data.sort_values(by=['tanggal'], ascending=False)
-df = data['option'].value_counts().rename_axis('option').reset_index(name='counts')
+__login__obj = __login__(auth_token = "courier_auth_token",
+                    company_name = "Shims",
+                    width = 200, height = 250,
+                    logout_button_name = 'Logout', hide_menu_bool = False,
+                    hide_footer_bool = False,
+                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
-fig1, ax1 = plt.subplots(figsize=(3,3))
-ax1.pie(df.counts, autopct='%1.1f%%', labels = df.option, colors = ['#FF5E63', '#3CC0FE'], startangle=90)
+LOGGED_IN= __login__obj.build_login_ui()
 
-st.subheader("**Kepuasan**")
-st.pyplot(fig1)
 
-st.subheader('Ceritanya')
+if st.session_state['LOGGED_IN'] == True:
+    st.markdown("Your Streamlit Application Begins here!")
+  
+    st.write(__login__obj.cookies['__streamlit_login_signup_ui_username__'])
+  
+    #Place Tet into Image
+    image = Image.open('frames.png')
+    path_font = "Inter-Regular.ttf"
 
-for j,k,l in zip(data['cerita'], data['option'], data['tanggal']):
-    cerita_list = list(j.split(" "))
-    if k == 'Iya':
-        st.info(f'**{l.date()}: {" ".join(cerita_list[:5]).title()}**')
-    else:
-        st.error(f'**{l.date()} : {" ".join(cerita_list[:5]).title()}**')
-    st.write(j)
+    font = ImageFont.truetype(path_font, 200)
+    font1 = ImageFont.truetype(path_font, 65)
+
+    img= ImageDraw.Draw(image)
+    img.text((170,314), str(rdelta.years), font=font, fill=(91,112,143))
+    img.text((573,304), str(rdelta.months), font=font1, fill=(91,112,143))
+    img.text((573,420), str(rdelta.days), font=font1, fill=(91,112,143))
+    img.text((573,539), str(rdelta.hours + 7), font=font1, fill=(91,112,143))
+
+    st.image(image)
+
+    with st.form(key='form1', clear_on_submit=True):
+        
+        col1 = db.collection('daily')
+        st.write("Apakah aku puas dengan hari ini?")
+        option = st.radio(' ', ('Iya', 'Engga'), horizontal=True)
+        cerita = st.text_area(label='Yang aku dapat hari ini', height=300)
+        submit_button = st.form_submit_button(label='Kirim')
+        if submit_button:
+            col1.add({"option": option, "tanggal": hari, "cerita": cerita})
+            st.write('Terimakasih 👍')
+
+
+    doc = db.collection('daily')
+    datas = list(doc.stream())
+    list_random = list(map(lambda x: x.to_dict(), datas))
+    data = pd.DataFrame(list_random)
+    data = data.sort_values(by=['tanggal'], ascending=False)
+    df = data['option'].value_counts().rename_axis('option').reset_index(name='counts')
+
+    fig1, ax1 = plt.subplots(figsize=(3,3))
+    ax1.pie(df.counts, autopct='%1.1f%%', labels = df.option, colors = ['#FF5E63', '#3CC0FE'], startangle=90)
+
+    st.subheader("**Kepuasan**")
+    st.pyplot(fig1)
+
+    st.subheader('Ceritanya')
+
+    for j,k,l in zip(data['cerita'], data['option'], data['tanggal']):
+        cerita_list = list(j.split(" "))
+        if k == 'Iya':
+            st.info(f'**{l.date()}: {" ".join(cerita_list[:5]).title()}**')
+        else:
+            st.error(f'**{l.date()} : {" ".join(cerita_list[:5]).title()}**')
+        st.write(j)
 
