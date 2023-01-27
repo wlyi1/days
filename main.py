578907e052d57f7b2e5da842c165df60b948edbc
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-27 15:21:50 +0700

    false footer

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ __login__obj = __login__(auth_token = "courier_auth_token",
                     company_name = "Shims",
                     width = 200, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = False,
-                    hide_footer_bool = True,
+                    hide_footer_bool = False,
                     lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
 LOGGED_IN= __login__obj.build_login_ui()
